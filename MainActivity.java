5af32884b535beb6401823f7581fbd8ebb4faa84
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2023-10-11 05:12:00 +0530

    Ex2

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,15 @@
-package com.example.buttonclickapp;
+package com.example.textviewcolorsizechange;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button myButton;
+    TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myButton = findViewById(R.id.myButton);
+        myText = findViewById(R.id.myText);
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Change the text of the button when clicked
-                myButton.setText("Text Changed");
+                // Change the background color
+                myText.setBackgroundColor(Color.BLUE);
+
+                // Change the text size
+                myText.setTextSize(24); // Change the text size to 24sp
             }
         });
     }
